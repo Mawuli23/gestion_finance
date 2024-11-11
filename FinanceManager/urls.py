0682a6c807b75065916cedc9356eee7d42bff782
Mawuli <skm.senyo@gmail.com>
2024-11-11 11:40:44 +0100

    Version 2

--- FinanceManager/urls.py
+++ FinanceManager/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('charges/', include('Charge.urls')),
     path('bilan-financier/', include('Bilan.urls')),
     path('accounts/', include('Accounts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
